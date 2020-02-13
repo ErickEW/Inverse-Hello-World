dce3ad4f13d45afb31a13d479a0ba2693b327409
ErickEW <60982154+ErickEW@users.noreply.github.com>
2020-02-13 15:29:13 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Inverse_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string helloWorld = "Hello World!";
+            int charCount = "Hello World!".Length;
+            char[] caracterHelloWorld = new char[charCount];
+            Console.WriteLine("Hello World!");
+
+            //caracterHelloWorld[0]
+
+            // Console.WriteLine(helloWorld);
+
+            // Console.WriteLine(helloWorld.Substring(0,5));
+
+            // Console.WriteLine(helloWorld[0]);
+
+            for (int i = 0; i < charCount; i++) {
+                Console.WriteLine(helloWorld[i]);
+
+                int newIndex = (helloWorld.Length - 1) - i;
+                caracterHelloWorld[i] = helloWorld[i];
+            }
+
+            Console.WriteLine(caracterHelloWorld);
+
+        }
+    }
+}
